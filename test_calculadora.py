52d0dd1caef2103d2512ee2952f757eedbc9e26e
Admin <alan.angulo@uabc.edu.mx>
2026-02-27 22:49:24 -0800

    Agregada prueba para multiply

--- test_calculadora.py
+++ test_calculadora.py
@@ -3,3 +3,7 @@ from calculadora import Calculadora
 def test_add():
     calc = Calculadora()
     assert calc.add(2, 3) == 5
+
+def test_multiply():
+    calc = Calculadora()
+    assert calc.multiply(2, 3) == 6
